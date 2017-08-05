bf45d0ffdcdacbac1d050f9fbfd6310951fac96d
Mat <mat@Mats-MacBook-Pro.local>
2017-08-05 16:58:00 -0700

    Most basic proof of concept possible

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isBreathing: false,
+    }
+  }
+
+  start() {
+    this.setState({isBreathing: true})
+  }
+
+  stop() {
+    this.setState({isBreathing: false})
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>i love you</Text>
+        { !this.state.isBreathing &&
+          <TouchableHighlight onPress={() => this.start()}>
+            <Image source={require('./circle.png')} />
+          </TouchableHighlight>
+        }
+        { this.state.isBreathing &&
+          <TouchableHighlight onPress={() => this.stop()}>
+            <Image source={require('./circle2.png')} />
+          </TouchableHighlight>
+        }
       </View>
     );
   }

10dd2cc60fd8812e04268d575920a8496a8b0107
Mat Geist <matgeist@gmail.com>
2017-08-07 20:22:28 -0700

    simplify logic

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Image, StyleSheet, TouchableHighlight, View } from 'react-native';
+import { Image, StyleSheet, TouchableHighlight } from 'react-native';
 
-import circle from './circle.png';
-import circle2 from './circle2.png';
+import startImage from './circle.png';
+import breathingImage from './circle2.png';
 
 const styles = StyleSheet.create({
-  container: {
+  image: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
@@ -19,30 +18,20 @@ export default class App extends React.Component {
     this.state = {
       isBreathing: false,
     };
+
+    this.toggleIsBreathing = this.toggleIsBreathing.bind(this);
   }
 
-  start() {
-    this.setState({ isBreathing: true });
-  }
-
-  stop() {
-    this.setState({ isBreathing: false });
+  toggleIsBreathing() {
+    this.setState({ isBreathing: !this.state.isBreathing });
   }
 
   render() {
+    const circleImage = this.state.isBreathing ? breathingImage : startImage;
     return (
-      <View style={styles.container}>
-        { !this.state.isBreathing &&
-          <TouchableHighlight onPress={() => this.start()}>
-            <Image source={circle} />
-          </TouchableHighlight>
-        }
-        { this.state.isBreathing &&
-          <TouchableHighlight onPress={() => this.stop()}>
-            <Image source={circle2} />
-          </TouchableHighlight>
-        }
-      </View>
+      <TouchableHighlight style={styles.image} onPress={this.toggleIsBreathing}>
+        <Image source={circleImage} />
+      </TouchableHighlight>
     );
   }
 }

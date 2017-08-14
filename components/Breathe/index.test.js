8aeda70d5755774b8693669db16b6a4fa96325c9
Mat Geist <matgeist@gmail.com>
2017-08-13 19:22:17 -0700

    downgraded some versions to get tests running, added tests

--- components/Breathe/index.test.js
+++ components/Breathe/index.test.js
@@ -16,6 +16,26 @@ describe('<Breathe />', () => {
 
     const { isBreathing, widthAnim, widthAnimValue } = instance.state;
     expect(isBreathing).toEqual(false);
-    expect(widthAnim).toEqual(Animated.Value(1));
+    expect(widthAnimValue).toEqual(new Animated.Value(1));
+    expect(widthAnim).toHaveProperty('start');
+  });
+
+  it('toggleIsBreathing', () => {
+    const wrapper = shallow(<Breathe />);
+    const instance = wrapper.instance();
+
+    const { widthAnim, widthAnimValue } = instance.state;
+    widthAnimValue.resetAnimation = jest.fn();
+    widthAnim.start = jest.fn();
+
+    instance.toggleIsBreathing();
+    expect(widthAnimValue.resetAnimation.mock.calls.length).toBe(1);
+    expect(widthAnim.start).toHaveBeenCalled();
+    expect(instance.state.isBreathing).toEqual(true);
+
+    instance.toggleIsBreathing();
+    expect(widthAnimValue.resetAnimation.mock.calls.length).toBe(2);
+    expect(widthAnim.start.mock.calls.length).toBe(1);
+    expect(instance.state.isBreathing).toEqual(false);
   });
 });

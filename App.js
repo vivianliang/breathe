13af3561000989a8fac90c6ac535f84ee4aaa57b
Vivian Liang <vliang88@gmail.com>
2017-08-24 23:07:21 -0700

    Swipe navigation

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Swiper from 'react-native-swiper';
 
 import Breathe from './components/Breathe';
+import Journey from './components/Journey';
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 const App = function App() {
-  return <Breathe />;
+  return (
+    <Swiper loop={false} showsPagination={false} index={0}>
+      <View style={styles.view}>
+        <Breathe />
+      </View>
+      <View>
+        <Journey />
+      </View>
+    </Swiper>
+
+  );
 };
 
 export default App;
